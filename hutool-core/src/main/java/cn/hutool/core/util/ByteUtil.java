5300119d622b767d26ba2473facc7f164fef0160
陈俊杰(002336) <32927876+TrillStones@users.noreply.github.com>
2023-02-02 16:35:42 +0800

    add todo

--- hutool-core/src/main/java/cn/hutool/core/util/ByteUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/util/ByteUtil.java
@@ -52,7 +52,7 @@ public class ByteUtil {
 	 */
 	public static int byteToUnsignedInt(byte byteValue) {
 		// Java 总是把 byte 当做有符处理；我们可以通过将其和 0xFF 进行二进制与得到它的无符值
-		return byteValue & 0xFF;
+		return byteValue & 0xFF;//todocjj
 	}
 
 	/**
